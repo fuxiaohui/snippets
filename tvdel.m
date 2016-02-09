8dee697d98533d140271080d27a2dba0ce59738a
fuxiaohui <fuxiaohui@126.com>
2016-02-09 13:39:15 +0800

    modify

--- tvdel.m
+++ tvdel.m
@@ -1,14 +1,15 @@
----
-title: "UITableViewDelegate"
-summary: "Placeholders for required UITableViewDelegate protocol methods"
-platform: iOS
-completion-scope: Class Implementation
----
+// UITableViewDelegate
+// Placeholders for required UITableViewDelegate protocol methods.
+//
+// IDECodeSnippetCompletionPrefix: tvdel
+// IDECodeSnippetCompletionScopes: [TopLevel]
+// IDECodeSnippetIdentifier: 4E041FB2-AB0D-4249-9658-141289641F6B
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetVersion: 2
 
 #pragma mark - UITableViewDelegate
 
-- (void)tableView:(UITableView *)tableView
-didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     <#statements#>
 }
